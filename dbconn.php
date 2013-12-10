d9475e4ac56aa51b932c024c8fd23ce1b0450124
wizardspro <wizardspro@live.com>
2013-12-10 02:20:12 +0000

    edited dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,3 +1,6 @@
 <?php
+echo "This is the connection string to the database.";
 mysql_connect('localhost','root','bitnami') or die(mysql_error());
 ?>
+<!DOCTYPE html>
+<html><head><title>DB Connection</title></head><body></body></html>
